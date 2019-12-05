1e16ce028010ca9a09772db4f1e88cf476c6ecf1
Christian Speckner <christian.speckner@mayflower.de>
2019-12-05 22:15:06 +0100

    Playback from SD working alongside display.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,30 @@
 #include <freertos/queue.h>
 #include <freertos/task.h>
 #include <cmath>
+#include <Adafruit_SSD1351.h>
+#include <Adafruit_GFX.h>
+
+#define PIN_CS_SD       GPIO_NUM_5
+#define PIN_CS_OLED     GPIO_NUM_15
+#define PIN_DC_OLED     GPIO_NUM_21
+#define PIN_RESET       GPIO_NUM_4
+#define PIN_I2S_BCK     GPIO_NUM_26
+#define PIN_I2S_WC      GPIO_NUM_22
+#define PIN_I2S_DATA    GPIO_NUM_25
+#define PIN_RFID_IRQ    GPIO_NUM_33
+#define PIN_CS_RFID     GPIO_NUM_2
+
+#define SPI_FREQ_SD     80000000
+#define SPI_FREQ_OLED   20000000
+
+#define PLAYBACK_CHUNK_SIZE 1024
+#define PLAYBACK_QUEUE_SIZE 8
+
+static SPIClass spiHSPI(HSPI);
+static SPIClass spiVSPI(VSPI);
 
 bool probeSd() {
-    if (!SD.begin(SS, SPI, 15000000)) {
+    if (!SD.begin(PIN_CS_SD, spiVSPI, SPI_FREQ_SD)) {
         Serial.println("Failed to initialize SD");
         return false;
     }
@@ -38,31 +59,10 @@ bool probeSd() {
     return true;
  }
 
-void audioThread(void* payload) {
-    void* buffer = malloc(1024);
+void fsStreamThread(void* payload) {
+    void* buffer = malloc(PLAYBACK_CHUNK_SIZE);
 
-    const i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-        .sample_rate = 44100,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL3,
-        .dma_buf_count = 10,
-        .dma_buf_len = 60,
-        .use_apll = false
-    };
-
-    const i2s_pin_config_t i2s_pins = {
-        .bck_io_num = GPIO_NUM_26,
-        .ws_io_num = GPIO_NUM_22,
-        .data_out_num = GPIO_NUM_25,
-        .data_in_num = -1
-    };
-
-    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-    i2s_set_pin(I2S_NUM_0, &i2s_pins);
-    i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
+    i2s_start(I2S_NUM_0);
 
     QueueHandle_t *queue = (QueueHandle_t*)payload;
     size_t bytes_written;
@@ -70,11 +70,11 @@ void audioThread(void* payload) {
     while (true) {
         xQueueReceive(*queue, buffer, portMAX_DELAY);
 
-        i2s_write(I2S_NUM_0, buffer, 1024, &bytes_written, portMAX_DELAY);
+        i2s_write(I2S_NUM_0, buffer, PLAYBACK_CHUNK_SIZE, &bytes_written, portMAX_DELAY);
     }
 }
 
-void playbackAudio() {
+void audioThread(void* payload) {
     File file = SD.open("/test.wav");
 
     if (!file) {
@@ -84,66 +84,100 @@ void playbackAudio() {
 
     file.seek(44);
 
-    int16_t* buffer = (int16_t*)malloc(1024);
+    int16_t* buffer = (int16_t*)malloc(PLAYBACK_CHUNK_SIZE);
 
-    QueueHandle_t audioQueue = xQueueCreate(4, 1024);
+    QueueHandle_t audioQueue = xQueueCreate(PLAYBACK_QUEUE_SIZE, PLAYBACK_CHUNK_SIZE);
 
     TaskHandle_t task;
-    xTaskCreatePinnedToCore(audioThread, "athread", 1024, (void*)&audioQueue, 10, &task, 1);
+    xTaskCreatePinnedToCore(fsStreamThread, "fsstream", 1024, (void*)&audioQueue, 1, &task, 1);
 
     size_t bytes_read = 0;
     size_t bytes_written;
 
     while (true) {
-        size_t bytes_read = file.read((uint8_t*)buffer, 1024);
+        size_t bytes_read = file.read((uint8_t*)buffer, PLAYBACK_CHUNK_SIZE);
 
-        if (bytes_read < 1024) {
+        if (bytes_read < PLAYBACK_CHUNK_SIZE) {
             file.seek(44);
-            file.read((uint8_t*)buffer + bytes_read, 1024 - bytes_read);
+            file.read((uint8_t*)buffer + bytes_read, PLAYBACK_CHUNK_SIZE - bytes_read);
         }
 
-        for (int i = 0; i < 512; i++) {
-            buffer[i] /= 8;
+        for (int i = 0; i < PLAYBACK_CHUNK_SIZE / 2; i++) {
+            buffer[i] /= 3;
         }
 
         xQueueSend(audioQueue, (void*)buffer, portMAX_DELAY);
     }
 }
 
+void runDisplay() {
+    Adafruit_SSD1351 tft = Adafruit_SSD1351(128, 128, &spiHSPI, PIN_CS_OLED, PIN_DC_OLED);
+    tft.begin(SPI_FREQ_OLED);
+
+    tft.fillScreen(0xFFFF);
+
+    while (true) {
+        int r = rand();
+        int x0 = r & 0x7f, y0 = (r >> 7) & 0x7f, x1 = (r >> 14) & 0x7f, y1 = (r >> 21) & 0x7f;
+
+        tft.drawLine(x0, y0, x1, y1, rand() & 0xffff);
+
+        taskYIELD();
+    }
+}
+
 void setup() {
+    pinMode(PIN_CS_RFID, OUTPUT);
+    pinMode(PIN_RESET, OUTPUT);
+    pinMode(PIN_CS_SD, OUTPUT);
+
+    digitalWrite(PIN_RESET, 0);
+    digitalWrite(PIN_CS_RFID, 1);
+    digitalWrite(PIN_CS_SD, 1);
+    digitalWrite(PIN_CS_OLED, 1);
+
+    delay(100);
+
+    digitalWrite(PIN_RESET, 1);
+
+    delay(100);
+
+    const i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = 44100,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL3,
+        .dma_buf_count = 4,
+        .dma_buf_len = 256,
+        .use_apll = false,
+        .tx_desc_auto_clear = true,
+    };
+
+    const i2s_pin_config_t i2s_pins = {
+        .bck_io_num = PIN_I2S_BCK,
+        .ws_io_num = PIN_I2S_WC,
+        .data_out_num = PIN_I2S_DATA,
+        .data_in_num = -1
+    };
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &i2s_pins);
+    i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
+    i2s_stop(I2S_NUM_0);
+
     Serial.begin(115200);
     Serial.println("Hello world!");
-
-    uint32_t* psram_ptr = (uint32_t*) ps_malloc(2* 1024 * 1024);
-
-    Serial.printf("allocated 2MB of PSRAM at %p\n", psram_ptr);
-
-    Serial.println("writing to PSRAM...");
-
-    int i;
-
-    for (i = 0; i < 512*1024; i++) psram_ptr[i] = i;
-
-    Serial.println("verifying PSRAM...");
-
-    for (i = 0; i < 512*1024; i++) {
-        if (psram_ptr[i] != i) {
-            Serial.printf("RAM differs at %i.\n", i);
-            break;
-        }
-    }
-
-    if (i == 512*1024) {
-        Serial.println("RAM verified OK");
-    }
-
-    free(psram_ptr);
 
     if (!probeSd()) {
         return;
     }
 
-    playbackAudio();
+    TaskHandle_t audioTask;
+    xTaskCreatePinnedToCore(audioThread, "playback", 4098, NULL, 10, &audioTask, 1);
+
+   runDisplay();
 }
 
 void loop() {}
