d3040c0417a8c0e5a9677dca18ee3f20a6ec3538
Christian Speckner <christian.speckner@mayflower.de>
2020-08-31 00:35:51 +0200

    Put all GPIOs in high impedance during sleep in order to avoid phantom power to the peripherials.

--- src/Power.cxx
+++ src/Power.cxx
@@ -15,6 +15,8 @@
 #include "Lock.hxx"
 #include "config.h"
 
+#include <Arduino.h>
+
 namespace {
 
 SemaphoreHandle_t mutex;
@@ -27,6 +29,13 @@ void Power::deepSleep() {
     prepareSleep();
 
     Lock lock(mutex);
+
+    for (auto pin :
+         {PIN_SD_CS, PIN_I2S_BCK, PIN_I2S_DATA, PIN_I2S_WC, PIN_RFID_CS, PIN_MCP23S17_CS, GPIO_NUM_14, GPIO_NUM_12,
+          GPIO_NUM_13, GPIO_NUM_18, GPIO_NUM_19, GPIO_NUM_23, PIN_RFID_IRQ, PIN_MCP23S17_IRQ, PIN_LED, PIN_POWER})
+        pinMode(pin, INPUT);
+
+    gpio_deep_sleep_hold_en();
 
     esp_sleep_enable_ext1_wakeup(static_cast<uint64_t>(1) << (PIN_WAKEUP - GPIO_NUM_32 + 32), ESP_EXT1_WAKEUP_ANY_HIGH);
     esp_deep_sleep_start();
